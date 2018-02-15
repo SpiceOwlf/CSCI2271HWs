41913c27970a7c81273d4b79779f3f89f5f189f4
SpicyOwl <duanxu@bc.edu>
2018-02-15 00:03:57 -0500

    Create HW2PartA.c
    this should work, not ultimate version yet

---
+++ HW2/HW2PartA.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX 80
+char *getCharBlock(int *size){
+  int index=0;
+  char *p;
+  char item;
+  p=(char*) malloc(sizeof(char));
+    printf("Enter a line here!\n");
+      item=getchar();
+    //start here
+  while (item!='\n'&& index<MAX){
+      *(p+index)=item;
+      index++;
+      p=(char*)realloc(p,index+1);
+      item=getchar();}
+
+    *(p+index)='\0';
+
+
+    *size=index;
+      return p;
+
+  }
+
+  //this while function need to be adjusted
+
+
+//////////////////////////////////////////////////////////////////
+
+int findMatch(char *text,int sizeText, char *pattern, int sizePattern){
+    int i1;
+    int i2=0;
+    int result;
+    for(i1=0;i1<sizeText;i1++){
+        if(*(text+i1)==*(pattern+i2)){
+          i2++;
+          if(i2==sizePattern){
+            break;
+          }
+        }
+        else if (*(text+i1)!=*(pattern+i2)){
+          i2=0;
+          if(*(text+i1)==*(pattern+i2)){
+            i2++;
+          }
+        }
+    }
+    if(i2==sizePattern){
+      result= 1;
+      printf("YES, we DO find the pattern\n" );
+    }
+    else {
+      result= 0;
+      printf("NO, we DON'T find the pattern\n" );
+    }
+    return result;
+}
+
+
+void printIt(char *ptr, int size){//why do we need index here?
+  int i;
+  for(i=0;i<size;i++){
+    printf("%c",*(ptr+i));
+  }
+  printf("\n" );
+}
+
+//////////////////////////////////////////////////////////////////
+int main(){
+char *text, *pattern; //pointers for the characters you will read
+char *p,*q,*r; //some pointer variables
+int size,size1,x,y; //some integers
+printf("give me your TEXT strings\n" );
+text= getCharBlock(&size);
+printf("the TEXT char is\n");
+printIt( text,size);
+printf("give me your PATTERN strings\n" );
+pattern=getCharBlock(&size1);
+printf("the PATTERN char is\n");
+printIt(pattern,size1);
+printf("So, let's see if we can find the pattern: \t" );
+findMatch(text,size,pattern,size1);
+//int findMatch(char *text,int sizeText, char *pattern, int sizePattern)
+free(text);
+free(pattern);
+}
